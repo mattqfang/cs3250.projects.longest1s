9b3ba0e6384392db622859c6c52b40d2bd6c9000
Unknown <skyesladeut@outlook.com>
2019-09-23 18:40:51 -0600

    Created Function and Tests

--- src/main/java/cs3250/projects/longest1s/Longest1s.java
+++ src/main/java/cs3250/projects/longest1s/Longest1s.java
@@ -2,6 +2,24 @@ package cs3250.projects.longest1s;
 
 public class Longest1s {
 	public static int getLongestRun(int num) {
-		return 0;
+		if(num < 0) {
+			return 0;
+		}
+		String binaryNum = Integer.toBinaryString(num);
+		System.out.println(binaryNum);
+		int numberOfOnes = 0;
+		int maxNumberOfOnes = 0;
+		for(int i = 0; i < binaryNum.length(); i++) {
+			if(binaryNum.charAt(i) == '1') {
+				numberOfOnes++;
+				if(numberOfOnes > maxNumberOfOnes) {
+					maxNumberOfOnes = numberOfOnes;
+				}
+			}
+			else {
+				numberOfOnes = 0;
+			}
+		}
+		return maxNumberOfOnes;
 	}
 }
--- src/test/java/cs3250/projects/longest1s/Longest1sTest.java
+++ src/test/java/cs3250/projects/longest1s/Longest1sTest.java
@@ -4,8 +4,23 @@ import static org.junit.Assert.assertEquals;
 
 public class Longest1sTest {
 	@Test
-	public void testGetLongest() {
-		assertEquals(0, Longest1s.getLongestRun(1));
+	public void testGetLongest_negativeNum() {
+		assertEquals(0, Longest1s.getLongestRun(-10));
+	}
+	
+	@Test
+	public void testGetLongest_numFourteen() {
+		assertEquals(3, Longest1s.getLongestRun(14));
+	}
+	
+	@Test
+	public void testGetLongest_numOneHundredOne() {
+		assertEquals(2, Longest1s.getLongestRun(101));
+	}
+	
+	@Test
+	public void testGetLongest_num() {
+		assertEquals(5, Longest1s.getLongestRun(31));
 	}
 	
 }
