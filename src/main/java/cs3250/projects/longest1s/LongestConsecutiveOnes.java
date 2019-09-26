917c4d1c36a6dd8432579d0687fc3dbeb723e9a4
Ben Hughes <xcyt21@yahoo.com>
2019-09-25 22:24:13 -0600

    removed check if number was 0, foreach catches it

--- src/main/java/cs3250/projects/longest1s/LongestConsecutiveOnes.java
+++ src/main/java/cs3250/projects/longest1s/LongestConsecutiveOnes.java
@@ -2,10 +2,7 @@ package cs3250.projects.longest1s;
 
 public class LongestConsecutiveOnes {
 
-	public static int countConsecutiveBinaryOnesFromInt(int num) {
-		if(num == 0)
-			return 0;
-		
+	public static int countConsecutiveBinaryOnesFromInt(int num) {		
 		String binaryString = Integer.toBinaryString(num);
 		String[] stringArray = binaryString.split("0");
 		int maxLength = 0;
