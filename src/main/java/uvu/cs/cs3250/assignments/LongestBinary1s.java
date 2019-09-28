795e084a719d6cbd40a7e7595cacee4ddbea9130
Brandenfascher <jonortim@hotmail.com>
2019-09-27 18:28:22 -0600

    First implementation
    need to add more unit tests

--- src/main/java/uvu/cs/cs3250/assignments/LongestBinary1s.java
+++ src/main/java/uvu/cs/cs3250/assignments/LongestBinary1s.java
@@ -2,8 +2,24 @@ package uvu.cs.cs3250.assignments;
 
 public class LongestBinary1s {
 	
-	public static int getLongestBinary1s(int number)
-	{
-		return 1;
+	public static int getLongestBinary1s(int number) {
+		String binaryString = Integer.toBinaryString(number);
+		
+		int totalLongestConsecutive1sCount = 0;
+		int currentConsecutive1sCount = 0;
+		
+		for(int i = 0; i < binaryString.length() ; i++) {
+			if(binaryString.charAt(i) == '1') {
+				currentConsecutive1sCount++;
+				
+				if(currentConsecutive1sCount > totalLongestConsecutive1sCount) {
+					totalLongestConsecutive1sCount++;
+				}
+			} else {
+				currentConsecutive1sCount = 0;
+			}
+		}
+		
+		return totalLongestConsecutive1sCount;
 	}
 }
