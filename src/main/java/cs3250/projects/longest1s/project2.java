18a778a5af45f469cf9d0ddd611f717d741c6041
zhimingzhu <10844118@my.uvu.edu>
2019-10-21 17:09:09 -0600

    project2

--- src/main/java/cs3250/projects/longest1s/project2.java
+++ src/main/java/cs3250/projects/longest1s/project2.java
@@ -8,21 +8,22 @@ public class project2 {
 		int count = 0;
 		int max = 0;
 		for (int i = 0; i < x.length(); i++) {
-			if (x.charAt(i) == 1) {
+			if (x.charAt(i) == '1') {
 				count = count + 1;
 			}
-			else if (x.charAt(i) == 0) {
-				if (count != 0) {
+			else if (x.charAt(i) == '0') {
+				if (count != 0 && max < count) {
 					max = count;
 				}
 				count = 0;
 			}
 		}
-		System.out.println(args + "is " + x);
+		System.out.println(args + " is " + x);
 		System.out.println("the longest consecutive set bits is");
 		for (int i = 0; i < max; i++) {
 			System.out.print("1");
 		}
+		System.out.println();
 		System.out.println("the length is " + max);
 	}
 
--- src/test/java/cs3250/projects/longest1s/test.java
+++ src/test/java/cs3250/projects/longest1s/test.java
@@ -1,13 +1,18 @@
 package cs3250.projects.longest1s;
 
+import org.junit.Test;
+
 public class test {
+	@Test
 	public void test1() {
 		project2.p2(10);
 	}
+	@Test
 	public void test2() {
 		project2.p2(50);
 	}
+	@Test
 	public void test3() {
-		project2.p2(100);
+		project2.p2(14);
 	}
 }
