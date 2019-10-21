d68128952ea30f6bfdc00fae7146c561c2a9d077
zhimingzhu <10844118@my.uvu.edu>
2019-10-21 16:47:02 -0600

    project2

---
+++ src/main/java/cs3250/projects/longest1s/project2.java
@@ -0,0 +1,29 @@
+package cs3250.projects.longest1s;
+
+public class project2 {
+
+	public static void p2(int args) {
+		// TODO Auto-generated method stub
+		String x = Integer.toBinaryString(args);
+		int count = 0;
+		int max = 0;
+		for (int i = 0; i < x.length(); i++) {
+			if (x.charAt(i) == 1) {
+				count = count + 1;
+			}
+			else if (x.charAt(i) == 0) {
+				if (count != 0) {
+					max = count;
+				}
+				count = 0;
+			}
+		}
+		System.out.println(args + "is " + x);
+		System.out.println("the longest consecutive set bits is");
+		for (int i = 0; i < max; i++) {
+			System.out.print("1");
+		}
+		System.out.println("the length is " + max);
+	}
+
+}
---
+++ src/test/java/cs3250/projects/longest1s/test.java
@@ -0,0 +1,13 @@
+package cs3250.projects.longest1s;
+
+public class test {
+	public void test1() {
+		project2.p2(10);
+	}
+	public void test2() {
+		project2.p2(50);
+	}
+	public void test3() {
+		project2.p2(100);
+	}
+}
