e37fd34eecf901344441eb4e7467cf84bf61b276
mccrakken <mccrakken77@gmail.com>
2019-10-22 18:37:03 -0600

    Find sequences in middle of string

--- src/main/java/uvu/cs3250/longest/Longest.java
+++ src/main/java/uvu/cs3250/longest/Longest.java
@@ -7,7 +7,7 @@ public class Longest {
         int count = 0;
         for(int i = 0; i < strNum.length(); i++){
             if (strNum.charAt(i) != '1')
-                break;
+                continue;
             int charCount = 1;
             for (int j = i + 1; j < strNum.length(); j++){
                 if (strNum.charAt(i) != strNum.charAt(j))
@@ -17,10 +17,10 @@ public class Longest {
             if (charCount > count)
                 count = charCount;
         }
-        return strNum + ": Longest sequence of 1\'s is " + count;
+        return strNum + ": " + count;
     }
     public static void main(String[] args){
-        int n = -1240231;
+        int n = 8955;
         System.out.println(longest(n));
         System.out.println(longest('b'));
     }
