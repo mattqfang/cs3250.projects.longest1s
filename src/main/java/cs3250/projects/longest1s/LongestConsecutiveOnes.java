fb0075598017847df1c8f7025a705ec322620097
ben hughes <ben.hughes815@gmail.com>
2019-11-07 15:19:22 -0700

    removed static from numerous places

--- src/main/java/cs3250/projects/longest1s/LongestConsecutiveOnes.java
+++ src/main/java/cs3250/projects/longest1s/LongestConsecutiveOnes.java
@@ -1,8 +1,8 @@
 package cs3250.projects.longest1s;
 
 public class LongestConsecutiveOnes {
-
-	public static int countConsecutiveBinaryOnesFromInt(int num) {		
+	
+	public int countConsecutiveBinaryOnesFromInt(int num) {		
 		String binaryString = Integer.toBinaryString(num);
 		String[] stringArray = binaryString.split("0");
 		int maxLength = 0;
--- src/test/java/cs3250/projects/longest1s/LongestConsecutiveOnesTest.java
+++ src/test/java/cs3250/projects/longest1s/LongestConsecutiveOnesTest.java
@@ -2,41 +2,48 @@ package cs3250.projects.longest1s;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class LongestConsecutiveOnesTest {
+	private LongestConsecutiveOnes lco;
+	
+	@Before
+	public void setUp() {
+		lco = new LongestConsecutiveOnes();
+	}
 	@Test
 	public void testNumber_0_LongestConsecutiveNumberOfOnes_0() {
-		assertEquals(0, LongestConsecutiveOnes.countConsecutiveBinaryOnesFromInt(0));
+		assertEquals(0, lco.countConsecutiveBinaryOnesFromInt(0));
 	}
 	
 	@Test
 	public void testNumber_14_LongestConsecutiveNumberOfOnes_3() {
-		assertEquals(3, LongestConsecutiveOnes.countConsecutiveBinaryOnesFromInt(14));
+		assertEquals(3, lco.countConsecutiveBinaryOnesFromInt(14));
 	}
 	
 	@Test
-	public void testNumber_Max_Value_LongestConsecutiveNumberOfOnes_31() { //2147483647
-		assertEquals(31, LongestConsecutiveOnes.countConsecutiveBinaryOnesFromInt(Integer.MAX_VALUE));
+	public void testNumber_Max_Value_LongestConsecutiveNumberOfOnes_31() {
+		assertEquals(31, lco.countConsecutiveBinaryOnesFromInt(Integer.MAX_VALUE));
 	}
 	
 	@Test
 	public void testNumber_239_LongestConsecutiveNumberOfOnes_4() {
-		assertEquals(4, LongestConsecutiveOnes.countConsecutiveBinaryOnesFromInt(239));
+		assertEquals(4, lco.countConsecutiveBinaryOnesFromInt(239));
 	}
 	
 	@Test
 	public void testNumber_235_LongestConsecutiveNumberOfOnes_3() {
-		assertEquals(3, LongestConsecutiveOnes.countConsecutiveBinaryOnesFromInt(235));
+		assertEquals(3, lco.countConsecutiveBinaryOnesFromInt(235));
 	}
 	
 	@Test
 	public void testNumber_341_LongestConsecutiveNumberOfOnes_1() {
-		assertEquals(1, LongestConsecutiveOnes.countConsecutiveBinaryOnesFromInt(341));
+		assertEquals(1, lco.countConsecutiveBinaryOnesFromInt(341));
 	}
 	
     @Test
 	public void testNumber_2731_LongestConsecutiveNumberOfOnes_2() {
-		assertEquals(2, LongestConsecutiveOnes.countConsecutiveBinaryOnesFromInt(2731));
+		assertEquals(2, lco.countConsecutiveBinaryOnesFromInt(2731));
 	}
 }
